f0bc87982a4f310f09926edd0966e446d1740fb7
Luke Callaway <lacallaway98@gmail.com>
2025-01-31 15:20:48 -0600

    updated

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const CLIENT_PASSWORD = process.env.CLIENT_PASSWORD;
-const DATABASE_URI = process.env.DB_URI;
+const DATABASE_URI = process.env.DATABASE_URI;
 const PORT = process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
